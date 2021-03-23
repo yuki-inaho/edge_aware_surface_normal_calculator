4112e7818980653eaf7c52f00f2dc1a56218c7bf
yuki-inaho <yoshikawa@inaho.co>
2021-03-23 17:50:09 +0900

    Add LICENSE term

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Implementation of the algorithm written in below article.
-
-Fast and Accurate Normal Estimation by Efficient 3d Edge Detection
-http://publica.fraunhofer.de/documents/N-366490.html
-
+Implementation of [Fast and Accurate Normal Estimation by Efficient 3d Edge Detection](http://publica.fraunhofer.de/documents/N-366490.html) <br>
 (official implementation: https://github.com/ipa-rmb/cob_object_perception)
+
+# LICENSE
+include/surface_normal_calculator.h : LGPL license <br>
+other part : MIT license
--- include/surface_normal_calculator.h
+++ include/surface_normal_calculator.h
@@ -1,3 +1,38 @@
+/*!
+ Some part of below code is borrowed from 
+ https://github.com/ipa320/cob_object_perception/blob/indigo_dev/cob_surface_classification/common/include/cob_surface_classification/edge_detection.h
+
+ *****************************************************************
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer. \n
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution. \n
+ * - Neither the name of the Fraunhofer Institute for Manufacturing
+ * Engineering and Automation (IPA) nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission. \n
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
 #pragma once
 #include <opencv2/opencv.hpp>
 #include <omp.h>
@@ -300,7 +335,7 @@ private:
         scan_line_length_1 = std::min(scan_line_length_1, free_dist.val[0] - 1 - MIN_DISTANCE_TO_DEPTH_EDGE);
         scan_line_length_2 = std::min(scan_line_length_2, free_dist.val[1] - 1 - MIN_DISTANCE_TO_DEPTH_EDGE);
 
-        if ((scan_line_length_1 < min_line_width || MIN_SCAN_LINE_WIDTH_FRACTION_FROM_MAX * scan_line_length_1 < max_1) ||
+        if ((scan_line_length_1 < min_line_width || MIN_SCAN_LINE_WIDTH_FRACTION_FRlOM_MAX * scan_line_length_1 < max_1) ||
             (scan_line_length_2 < min_line_width || MIN_SCAN_LINE_WIDTH_FRACTION_FROM_MAX * scan_line_length_2 < max_2))
             return false;
         return true;
@@ -434,7 +469,7 @@ private:
         const int max_v = depth_image_z_dx_.rows - max_scan_line_width - 2;
         const int max_u = depth_image_z_dx_.cols - max_scan_line_width - 2;
 
-        #pragma omp parallel for
+#pragma omp parallel for
         for (int v = max_scan_line_width + 1; v < max_v; ++v)
         {
             int scan_line_width = scan_line_width_init;
@@ -521,7 +556,7 @@ private:
         const int max_uy = depth_image_z_dy_transposed.cols - max_scan_line_width - 2;
         const int max_vy = depth_image_z_dy_transposed.rows - max_scan_line_width - 2;
 
-        #pragma omp parallel for
+#pragma omp parallel for
         for (int v = max_scan_line_width + 1; v < max_vy; ++v)
         {
             int scan_line_width = scan_line_width_init; // width of scan line left or right of a query pixel, measured in [px]
@@ -659,7 +694,7 @@ private:
                 const float depth = z_image.at<float>(v, u);
                 if (depth == 0.f || edge_.at<uchar>(v, u) != 0 || v <= max_scan_line_width || v >= max_v || u <= max_scan_line_width || u >= max_u)
                 {
-                    normal_.at<cv::Vec3f> (v, u) = {0, 0, 0};
+                    normal_.at<cv::Vec3f>(v, u) = {0, 0, 0};
                     continue;
                 }
 
